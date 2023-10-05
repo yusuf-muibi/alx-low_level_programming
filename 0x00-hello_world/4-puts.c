2e54ff9ba92839576e588df3b56b1ab0ae2cc4b3
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-05 07:50:07 -0700

    Hello ,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - main is the main function in use
+ * Return: always 0
+*/
+
 int main(void)
 {
-char myput[] = "\"Programming is like building a multilingual puzzle\n";
+char myput[] = "Programming is like building a multilingual puzzle";
 puts(myput);
 return (0);
 }
