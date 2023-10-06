d74cf3a1c3c708b063d09a390029be8d036e9c57
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-06 05:48:45 -0700

    Task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+*main - main is the main function used in the code
+*Return: will always return zero for a successful execution
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
