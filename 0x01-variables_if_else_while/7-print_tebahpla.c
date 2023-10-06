8e460b8f9a51d2ada497127484b0e405917b8504
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-06 14:29:15 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - This is the main function
+ *Return: Always returns the zero value for a successful execution
+ */
+int main(void)
+{
+char t;
+for (t = 'z' ; t >= 'a' ; t--)
+putchar(t);
+putchar('\n');
+return (0);
+}
