fca56400f7772835236aed0bb6368dc841884b98
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-06 14:23:13 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main function of the program
+ * Return: Always return zero for a successful execution
+ */
+int main(void)
+{
+int t;
+for (t = 0; t < 10 ; t++)
+putchar(t + '0');
+putchar('\n');
+return (0);
+}
