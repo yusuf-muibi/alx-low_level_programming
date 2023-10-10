e7219f635cc41a3bf10e5a4247ad6f0a2dfe3d7d
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-10 04:35:17 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - This is the main function being used in the program
+* Return: Always return zero for a successful execution
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
