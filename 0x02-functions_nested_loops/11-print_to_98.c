de2aa22f192a67e29300ccada9aff8278bb60d38
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-10 14:37:29 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - This is the main functon in use
+* @n: This is the parameter in use
+* Return: Always return zero for a successful execution
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
