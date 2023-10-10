09f8d3541b3a06fb645925da4f4cdb9570a7f42d
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-10 06:42:53 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - this is the main function used in the program
+* Return: Always returns a 0 for a successful execution
+*/
+void print_alphabet(void);
+{
+char t;
+for (t = 'a' ; t <= 'z' ; t++)
+{
+_putchar(t);
+_putchar("\n");
+}
+}
