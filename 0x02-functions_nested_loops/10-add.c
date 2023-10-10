58cb345000420edba25c25cb32c16a741f2b6030
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-10 13:24:26 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - Main function to sum number
+* @t: First parameter
+* @u: Second parameter
+* Return: Returns the result
+*/
+int add(int t, int u)
+{
+return (t + u);
+}
