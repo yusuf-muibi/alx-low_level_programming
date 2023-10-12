a42e7821a0ef09987f52eaf0262455700707af3e
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-12 08:55:21 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_diagonal - this is the function that performs the printing
+* @n: the determinant of how long the line will be
+* Return: returns a void value
+*/
+
+void print_diagonal(int n)
+{
+if (n <= 0)
+_putchar('\n');
+else
+{
+int t, u;
+for (t = 0; t < n ; t++)
+{
+for (u = 0; u <= t; u++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
