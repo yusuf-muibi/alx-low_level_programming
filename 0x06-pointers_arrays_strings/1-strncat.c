be59160969e87a104d44ace3bf42181df54708ef
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-19 08:27:37 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
 * _strncat - this is the function that concatenates two string
 * @dest: this is the destination string
 * @src: this is the source string
+* @n: this is the parameter for the length of string
 * Return: this returns the destination string
 */
 char *_strncat(char *dest, char *src, int n)
@@ -18,5 +19,5 @@ for (t = 0; t < n && src[t] != '\0'; t++)
 destEnd[t] = src[t];
 }
 destEnd[n] = '\0';
-return dest;
+return (dest);
 }
