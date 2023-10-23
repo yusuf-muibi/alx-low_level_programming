ed96fcf43a9e35d14101e2adca1721e124508c89
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-23 07:52:50 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_chessboard - this is the function that prints the chessboard
+* @a: this is the number of row
+* Return: this returns the printed chessboard
+*/
+void print_chessboard(char (*a)[8])
+{
+int t, u;
+for (t = 0; t < 8; t++)
+{
+for (u = 0; u < 8; u++)
+{
+  printf("%c", a[t][u]);
+}
+printf("\n");
+}
+}
