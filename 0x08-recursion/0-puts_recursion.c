04d05447ff32af6361fb8f3007de6ae679ad8f57
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-25 06:43:37 -0700

    Task 1 of recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - This is the function used to call the recursion function
+* @s: This is the character to be printed
+* Return: This returns the string of characters
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
