296573414a42f74ef5bfa41a7a62d755f95b3691
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-27 08:51:47 -0700

    Bureau de Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - the main function in the program
+* @argc: this is the number of arguments
+* @argv: this is the array of the arguments
+* Return: returns zero for a successful execution
+*/
+int main(int argc, char *argv[])
+{
+int cents;
+int coins;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+coins = 0;
+
+coins += cents / 25;
+cents %= 25;
+
+coins += cents / 10;
+cents %= 10;
+
+coins += cents / 5;
+cents %= 5;
+
+coins += cents / 2;
+cents %= 2;
+
+coins += cents;
+printf("%d\n", coins);
+return (0);
+}
