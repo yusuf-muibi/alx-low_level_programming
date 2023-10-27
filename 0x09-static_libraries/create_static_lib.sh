e0676289c8e2d9dffd8ae65f222c96bf317a8e63
yusuf-muibi <muibiyusuf@hotmail.com>
2023-10-27 06:34:23 -0700

    Script to create library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-c_files=$(ls *.c)
-for source_file in $c_files;
-do
-object_file="${source_file%.c}.o"
-gcc -c "$source_file" -o "$object_file"
-done
-
-
-ar rcs liball.a *.o
-
-rm -f *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
