68b45fed19c700c9955060dd0a65ee808ec5431c
yusuf-muibi <muibiyusuf@hotmail.com>
2023-11-02 06:21:51 -0700

    Realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* _realloc - function that reallocates a memory block
+* @ptr: the memory pointer
+* @new_size: this is the new memory size
+* @old_size: this is the old memory size
+* Return: returns pointer to the reallocated memory block
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *new_ptr;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+return (malloc(new_size));
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+return (NULL);
+if (new_size > old_size)
+memcpy(new_ptr, ptr, old_size);
+else
+memcpy(new_ptr, ptr, new_size);
+free(ptr);
+return (new_ptr);
+}
