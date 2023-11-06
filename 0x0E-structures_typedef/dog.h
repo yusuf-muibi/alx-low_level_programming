bc5714f681c713c9105b4c5c756a277172e9246d
yusuf-muibi <muibiyusuf@hotmail.com>
2023-11-06 06:15:41 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - this is the main function
+* @name: this is the name of the dog
+* @age: this is the age of the dog
+* @owner: this is the name of the owner
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
